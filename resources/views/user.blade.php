e3fc07a86301f8c107af9062b8a7f11b8338b8ff
= Ryayaa <= arrya306@gmail.com>
2024-03-05 10:18:06 +0700

    update

---
+++ resources/views/user.blade.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        <table border="1" cellpadding="2" cellsapcing="0">
+            <tr>
+                <td>ID</td>
+                <td>Username</td>
+                <td>Nama</td>
+                <td>ID Level Pengguna</td>
+            </tr>
+            <tr>
+                <td>{{ $data->user_id }}</td>
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->level_id }}</td>
+            </tr>
+        </table>
+    </body>
+</html>
