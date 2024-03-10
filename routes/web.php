4b9958d59332e7692f37a57b4238f460f8edd9e1
= Ryayaa <= arrya306@gmail.com>
2024-03-11 04:29:45 +0700

    praktikum 2.7

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,10 @@ Route::get('/Penjualan', [PenjualanController::class, 'index']);
 
 Route::get('level', [LevelController::class,'index']);
 Route::get('/kategori', [KategoriController::class,'index']);
+Route::get('/user/tambah', [UserController::class,'tambah']);
+
+Route::post('/user/tambah_simpan', [UserController::class,'tambah_simpan']);
+Route::get('/user/ubah{id}', [UserController::class,'ubah']);
+Route::put('/user/ubah_simpan/{id}', [UserController::class,'ubah_simpan']);
+Route::get('/user/hapus/{id}', [UserController::class,'hapus']);
+
