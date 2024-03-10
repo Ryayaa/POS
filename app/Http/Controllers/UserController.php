b82dce2da5a385e97e15662154545fc0048c311c
= Ryayaa <= arrya306@gmail.com>
2024-03-11 02:40:51 +0700

    praktikum 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,14 +12,21 @@ class UserController extends Controller
     {
        $user = UserModel::firstOrNew(
         [
-        'username' => 'manager33',
-        'nama' => 'Manager Tiga Tiga',
+        'username' => 'manager11',
+        'nama' => 'Manager11',
         'password' => Hash::make('12345'),
         'level_id' => 2
         ],
     );
+
+    $user->username = 'manager12';
+
     $user->save();
-        
-       return view ('user',  ['data' => $user]); 
+
+    $user->wasChanged();
+    $user->waschanged('username'); // true
+    $user->waschanged(['username', 'level_id']); // true
+    $user->waschanged('nama'); // false
+    dd($user->waschanged(['nama','username'])); // true
 }
 }
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -12,7 +12,9 @@
                 <td>{{ $data->username }}</td>
                 <td>{{ $data->nama }}</td>
                 <td>{{ $data->level_id }}</td>
+                <td><a href="/user/ubah/{{$d->user_id}}">Ubah</a> | <a href="/user/hapus{{$d->user_id}}">Hapus</a></td>
             </tr>
+            @endforeach
         </table>
     </body>
 </html>
