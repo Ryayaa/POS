fb21e5eb8334cfff25709a58c700e8fb01c45ae8
= Ryayaa <= arrya306@gmail.com>
2024-03-18 23:40:47 +0700

    praktikum 5

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Yajra\DataTables\DataTablesServiceProvider::class,
         /*
          * Application Service Providers...
          */
@@ -168,6 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+
     ])->toArray(),
 
     /*
@@ -184,5 +186,7 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
+
+
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,10 @@ use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/', function () {
+    return view('welcome');});
 
-Route::get('/Home', [HomeController::class, 'index']);
+// Route::get('/Home', [HomeController::class, 'index']);
 
 Route::get('/Products', [ProductsController::class, 'index']);
 
@@ -27,7 +29,7 @@ Route::get('/user', [UserController::class, 'index']);
 Route::get('/Penjualan', [PenjualanController::class, 'index']);
 
 Route::get('level', [LevelController::class,'index']);
-Route::get('/kategori', [KategoriController::class,'index']);
+// Route::get('/kategori', [KategoriController::class,'index']);
 Route::get('/user/tambah', [UserController::class,'tambah']);
 
 Route::post('/user/tambah_simpan', [UserController::class,'tambah_simpan']);
@@ -35,3 +37,13 @@ Route::get('/user/ubah{id}', [UserController::class,'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class,'ubah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class,'hapus']);
 
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/kategori', [KategoriController::class, 'index']);
+
+Route::get('/kategori/create', [KategoriController::class, 'create']);
+Route::post('/kategori', [KategoriController::class, 'store']);
+Route::get('/kategori/edit/{id}',[KategoriController::class, 'edit'])->name('kategori.edit');
+Route::put('/kategori/update/{id}',[KategoriController::class, 'update'])->name('kategori.update');
+Route::get('/kategori/delete/{id}',[KategoriController::class, 'delete'])->name('kategori.delete');
+Route::get('/kategori/create',[KategoriController::class, 'create'])->name('kategori.create');
